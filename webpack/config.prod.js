7d5ceb243369e01cc37a9a5060d733aac33e1217
Maxim Dikun <dikun.mv@gmail.com>
2018-05-28 18:09:30 +0300

    add webpack prod config

---
+++ webpack/config.prod.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const root = `${__dirname}/..`;
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: {
+    app: path.resolve(root, 'src/index.js'),
+    vendor: [
+      '@tensorflow-models/posenet',
+      '@tensorflow/tfjs',
+      'axios',
+      'stats.js'
+    ],
+  },
+  output: {
+    path: path.resolve(root),
+    filename: 'www/[name].[hash].js',
+    publicPath: './'
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin(['NODE_ENV']),
+    new HtmlWebpackPlugin({
+      template: path.resolve(root, 'src/index.html'),
+      favicon: path.resolve(root, 'src/favicon.ico'),
+      title: 'App Skeleton'
+    })
+  ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: 'vendor',
+          name: 'vendor',
+          enforce: true
+        }
+      }
+    }
+  }
+};
